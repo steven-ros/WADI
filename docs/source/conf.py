632946860dd0588b09c1dcf05588704e5bc5c8e0
Steven Ros <steven.ros@kwrwater.nl>
2022-06-21 20:47:40 +0200

    added plot directive to conf py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -36,7 +36,7 @@ master_doc = 'index'
 extensions = [
     "IPython.sphinxext.ipython_directive",
     "IPython.sphinxext.ipython_console_highlighting",
-    # "matplotlib.sphinxext.plot_directive",
+    "matplotlib.sphinxext.plot_directive",
     'sphinx.ext.napoleon',
     'sphinx.ext.autodoc',
     'sphinx_rtd_theme'
