7d58a86a0589d325d48cdb84c741f7aa5c091806
Steven Ros <steven.ros@kwrwater.nl>
2022-06-21 21:30:31 +0200

    0.2.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def read(fname):
 
 setup(
     name='sutra',
-    version='0.2.0',
+    version='0.2.1',
     packages=find_packages(exclude=['tests*']),
     license='MIT',
     description='A python package for calculating the removal of microbial organisms in the subsurface',
